368ed54e65f01b86688e9e543df1c5cd41bd47be
Maurits Seelen <mauritsseelen@hotmail.com>
2024-07-29 18:39:47 +0200

    messing around with some UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { WasmLoaderService } from './services/wasm-loader.service.js';
-import { FormsModule } from '@angular/forms';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { MarkdownComponent } from 'ngx-markdown';
+import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component.js';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, MarkdownComponent],
+  imports: [NavigationComponent, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  userInput: string = ''
-  parsedText: SafeHtml | undefined
-
-  private result: string[] = []
-  private wasmInstance: any
-
-  constructor(
-    private wasmLoaderService: WasmLoaderService,
-    private sanitizer: DomSanitizer) { }
-
-  async ngOnInit(): Promise<void> {
-    await this.wasmLoaderService.loadWasm()
-    this.wasmInstance = this.wasmLoaderService.getWasmInstance()
-  }
-
-  isButtonDisabled(): boolean {
-    return this.userInput.trim() === ''
-  }
-
-  runCode(): void {
-    try {
-      if (this.userInput.trim() === '') {
-        return
-      }
-
-      this.result = []
-      this.parsedText = undefined
-      this.pruneRes(this.wasmInstance.run_code(this.userInput))
-      console.log(this.result)
-    } catch (err) {
-      console.error("Something went wrong: ", err)
-    }
-  }
-
-  private pruneRes(result: string[]): void {
-    this.result = result.filter(item => item !== 'null')
-    let combined = this.result.join(" ")
-    this.parsedText = this.sanitizer.bypassSecurityTrustHtml(this.parseAnsiToHtml(combined))
-  }
-
-  private parseAnsiToHtml(text: string): string {
-    return text
-      .replace(/\x1b\[31m/g, '<span style="color: red;">')
-      .replace(/\x1b\[32m/g, '<span style="color: green;">')
-      .replace(/\x1b\[33m/g, '<span style="color: green;">')
-      .replace(/\x1b\[34m/g, '<span style="color: blue;">')
-      .replace(/\x1b\[35m/g, '<span style="color: magenta;">')
-      .replace(/\x1b\[36m/g, '<span style="color: cyan;">')
-      .replace(/\x1b\[37m/g, '<span style="color: white;">')
-      .replace(/\x1b\[0m/g, '</span>')
-  }
-}
+export class AppComponent { }
