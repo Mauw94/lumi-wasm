fd070e87a48d440dc2fbd86001db61a31f40c858
Maurits Seelen <mauritsseelen@hotmail.com>
2024-07-30 10:50:26 +0200

    Update header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component.js';
 import { RouterLink, RouterModule } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from './footer/footer.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent, RouterModule, RouterLink],
+  imports: [NavigationComponent, HeaderComponent, FooterComponent, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
