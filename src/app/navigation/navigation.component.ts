8cb9bc998cf2d6126f7f3089fdd86ca4e8692547
Maurits Seelen <mauritsseelen@hotmail.com>
2024-07-30 09:41:13 +0200

    add better navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,6 +8,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit {
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    const navContainer = this.renderer.selectRootElement('#nav', true)
+    const navLinks = navContainer.getElementsByClassName('nav-link')
+
+    for (let i = 0; i < navLinks.length; i++) {
+      this.renderer.listen(navLinks[i], 'click', (event) => {
+        const current = navContainer.getElementsByClassName('active')
+        if (current.length > 0) {
+          this.renderer.removeClass(current[0], 'active')
+        }
+        this.renderer.addClass(event.target, 'active')
+      })
+    }
+  }
 
 }
