81a3e10cd7c8e032ac8633694b7a6ff1c5615e04
Maurits Seelen <mauritsseelen@hotmail.com>
2024-11-14 20:27:23 +0100

    eh..

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     huggingFaceApiUrl: 'https://api-inference.huggingface.co/models/Artples/LAI-ImageGeneration-vSDXL-2',
-    huggingFaceApiKey: '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'
+    huggingFaceApiKey: ''
 }
