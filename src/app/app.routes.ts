08ea8ca33f1beec542e210520c36e6aaf65e692a
Maurits Seelen <mauritsseelen@hotmail.com>
2024-11-15 09:05:38 +0100

    Add blog and Notes component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LumiComponent } from './lumi/lumi.component';
 import { ImageGenComponent } from './image-gen/image-gen.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { NotesComponent } from './notes/notes.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'lumi', component: LumiComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'blogs', component: BlogsComponent },
+    { path: 'notes', component: NotesComponent },
     { path: 'image-gen', component: ImageGenComponent }
 ];
 
