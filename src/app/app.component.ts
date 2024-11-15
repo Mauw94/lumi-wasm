6469e4a1fe0aa0be85ca1f71cd602d1d708cd132
Maurits Seelen <mauritsseelen@hotmail.com>
2024-11-15 09:01:20 +0100

    prevent env dev from being commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component.js';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from './footer/footer.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent, HeaderComponent, FooterComponent, RouterModule, RouterLink],
+  imports: [NavigationComponent, HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    huggingFaceApiUrl: 'https://api-inference.huggingface.co/models/Artples/LAI-ImageGeneration-vSDXL-2',
-    huggingFaceApiKey: ''
-}
